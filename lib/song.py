ffd346255711a0d58b8f9ff22ba152c5c7272c06
Sharon Njoroge <waithirasharon222@gmail>
2025-11-30 20:16:58 +0300

    completed work

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,54 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    artists = []
+    genres = []
+    artist_count = {}
+    genre_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update class-level data
+        Song.add_song_to_count()
+        Song.add_to_artists(self.artist)
+        Song.add_to_genres(self.genre)
+        Song.add_to_artist_count(self.artist)
+        Song.add_to_genre_count(self.genre)
+
+    # Class method to increment total song count
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    # Class method to add unique artists
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    # Class method to add unique genres
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    # Class method to track number of songs per artist
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    # Class method to track number of songs per genre
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
